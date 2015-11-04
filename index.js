7d3375863379971f9838ea5d2cfdad51af83dee5
Jose Ayon <josalfredyon@gmail.com>
2015-11-04 18:19:58 -0500

    Agrego configuración predeterminada

--- index.js
+++ index.js
@@ -2,19 +2,33 @@
  * configuracion:
  *[
  *  {
- *    src: path.join(base, 'src', 'templates'),
- *    dest: path.join(path.join(base, 'dist'))
+ *    src: ['!src/templates/client/**', 'src/templates/*.jade'],
+ *    dest: 'dist'
  *  }
  *]
-*/
+ */
 
 var gulp = require('gulp');
+
+var path = require('path');
 
 var gulpif = require('gulp-if');
 
 var jade = require('gulp-jade');
 
 var htmlMin = require('gulp-htmlmin');
+
+var yargs = require('yargs').argv;
+
+var cwd = process.cwd();
+
+var default_config = {
+  src: [
+    '!' + path.join(cwd, 'src', 'templates', 'client', '**'),
+    path.join(cwd, 'src', 'templates', '*.jade')
+  ],
+  dest: path.join(cwd, 'dist')
+};
 
 function condition(file) {
   if (file.relative.indexOf('/_') !== -1 || file.relative.indexOf('_') === 0) {
@@ -26,17 +40,24 @@ function condition(file) {
 
 module.exports = function(configs) {
   gulp.task('templates', function() {
+    configs = configs || [default_config];
+
     configs.forEach(function(config) {
+      config.handler = config.handler || {};
+
+      config.yargs = yargs || {};
+
       gulp.src(config.src)
         .pipe(jade({
           locals: {
             handler: config.handler
           },
-          pretty: (config.yargs.prod) ? false : true
+          pretty: (yargs.prod) ? false : true
         }))
-        .pipe(gulpif(config.yargs.prod, htmlMin()))
-        .pipe(gulpif(condition, gulp.dest(config.dest)));
+
+      .pipe(gulpif(yargs.prod, htmlMin()))
+
+      .pipe(gulpif(condition, gulp.dest(config.dest)));
     });
   });
 };
-
